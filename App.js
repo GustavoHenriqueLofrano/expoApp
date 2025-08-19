99f6bc7a0587aea55f0aa48eb8f7ec2ffa5db826
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-08-19 10:30:43 -0300

    add

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
+export default function App(){
+  
+
+
+  const Calculate =()=>{
+    const num1 = parseFloat(num1)
+    const num2 = parseFloat(num2)
+    let result = 0
+
+    switch(operation) {
+      case '+':
+        result = num1 + num2
+        break;
+      case '-':
+        result = num1 - num2
+        break;
+      case '*':
+        result = num1 * num2
+        break;
+      case '/':
+        result = num1 / num2
+        break;
+      default:
+        result = 0
+    }
+    return result
+  }
+  const Clear=()=>{
+    setnum1('0');
+    setnum2('0');
+    setoperation('');
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+      </Text>
     </View>
-  );
+    
+  )
 }
 
-const styles = StyleSheet.create({
+const styles= StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
+  }
+})
